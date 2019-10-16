52e297848b0a983a5e419933996914c64fad0eb7
Ovidio Martinez <finrosdev@gmail.com>
2019-10-16 08:37:11 -0700

    fullFunctionality1

--- src/components/mainList.jsx
+++ src/components/mainList.jsx
@@ -1,43 +1,119 @@
 import React from 'react';
 import ImportedTasks from './tasks.json';
 import ListElement from './listElement';
-import './style.css'
-import {SortableContainer, SortableElement} from 'react-sortable-hoc';
+// import './style.css'
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-class MainList extends React.Component{
-    constructor (props){
-        super (props);
+const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: "none",
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+
+    // change background colour if dragging
+    background: isDragging ? "lightgreen" : "grey",
+
+    // styles we need to apply on draggables
+    ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? "lightblue" : "lightgrey",
+    padding: grid,
+    width: 250
+});
+
+class MainList extends React.Component {
+    constructor(props) {
+        super(props);
         this.state = {
-            tasks:[],
-            renderCount:5
+            tasks: '',
+            renderCount: 5
         }
+        this.onDragEnd = this.onDragEnd.bind(this);
     }
-    
-    increaseCount  = async () => {
+    onDragEnd(result) {
+        if (!result.destination) {
+            return;
+        }
+
+        const tasks = reorder(
+            this.state.tasks,
+            result.source.index,
+            result.destination.index
+        );
+
+        this.setState({
+            tasks
+        });
+    }
+
+    increaseCount = async () => {
         let count = this.state.renderCount
-    this.setState({renderCount:count+5}, this.renderArray())
-    console.log (this.state.renderCount)
+        this.setState({ renderCount: count + 5 }, this.renderArray())
+        console.log(this.state.renderCount)
     }
-    componentDidMount (){
+    componentDidMount() {
         this.renderArray()
     }
-    renderArray = ()=>{
-        let arrMax= this.state.renderCount;
-        let auxArr= JSON.parse(JSON.stringify(ImportedTasks));
-        this.setState({tasks: auxArr.slice(0,arrMax)})
+    renderArray = () => {
+        let arrMax = this.state.renderCount;
+        let auxArr = JSON.parse(JSON.stringify(ImportedTasks));
+        this.setState({ tasks: auxArr.slice(0, arrMax) })
         console.log('hi')
     }
-    renderChildren = ()=>{
-        return this.state.tasks.length? this.state.tasks.slice(0,this.state.renderCount).map((task, idx)=><ListElement key={idx} data={task}/>) : "";
-    }
-    render (){
-        return(
-            <div className= "MainList" >
-               <input type='button' value='See More' onClick={this.increaseCount}/>
-                {this.renderChildren()}
-            </div>
 
-        )
+    renderChildren = () => {
+        return this.state.tasks.length ? this.state.tasks.slice(0, this.state.renderCount).map((task, idx) =>
+            <Draggable key={task[idx]} draggableId={task[idx]} index={idx}>
+                {(provided, snapshot) => (
+                    <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style
+                        )}
+                    >
+                        <ListElement key={idx} data={task} />
+                    </div>
+                )}
+            </Draggable>
+        ) : "";
     }
+    render() {
+        return (
+            <div>
+                <input type='button' value='See More' onClick={this.increaseCount} />
+                <DragDropContext onDragEnd={this.onDragEnd}>
+                    <Droppable droppableId="droppable">
+                        {(provided, snapshot) => (
+                            <div
+                                {...provided.droppableProps}
+                                ref={provided.innerRef}
+                                style={getListStyle(snapshot.isDraggingOver)}
+                            >
+                                {this.renderChildren()}
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+            </div>
+        );
+    }
+
 }
+
 export default MainList;
